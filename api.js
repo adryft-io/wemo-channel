657ec53517311e03089705d91d9ae7e88eab4637
Nam Dao <nam.d.dao@gmail.com>
2016-07-06 19:28:56 -0700

    updated wemo api server to reduce socket connections

--- api.js
+++ api.js
@@ -7,26 +7,20 @@ AWS.config.credentials =  new AWS.SharedIniFileCredentials({ profile: 'newreacti
 AWS.config.update({region: 'us-east-1'});
 var Consumer = require('sqs-consumer');
 server.listen(8080);
-
-var isHost = function () {
-  return true;
-};
-
-io.sockets.on('connection', function (socket) {
-  var worker = Consumer.create({
-    queueUrl: process.env.queue_URL,
-    handleMessage: function (message, done) {
-      socket.emit("message", message.Body);
-      done();
-      console.log('message from sqs is', message.Body);
-    },
-    sqs: new AWS.SQS()
-  });
-  worker.on('error', (err) => {
-    console.log('err from app.js is', err.message);
-  });
-  worker.start();
+  
+var worker = Consumer.create({
+  queueUrl: process.env.queue_URL,
+  handleMessage: function (message, done) {
+    io.emit("message", message.Body);
+    done();
+    console.log('message from queue is', message.Body);
+  },
+  sqs: new AWS.SQS()
 });
+worker.on('error', (err) => {
+  console.log('err from app.js is', err.message);
+});
+worker.start();
 
 
 
