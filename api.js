4bca659d554236066b421e8399f2ef374069ef84
Nam Dao <nam.d.dao@gmail.com>
2016-07-07 14:27:19 -0700

    modified queue url

--- api.js
+++ api.js
@@ -6,17 +6,19 @@ var AWS = require('aws-sdk');
 AWS.config.update({region: process.env.AWS_REGION});
 var Consumer = require('sqs-consumer');
 server.listen(8080);
-  
-var worker = Consumer.create({
-  queueUrl: process.env.QUEUE_URL,
-  handleMessage: function (message, done) {
-    console.log('message from queue is', message.Body);
-    io.emit("message", message.Body);
-    done();
-  },
-  sqs: new AWS.SQS()
+
+sqs.getQueueUrl({ QueueName: 'wemo-channel' }, function(err, data) {  
+  var worker = Consumer.create({
+    queueUrl: data.QueueUrl,
+    handleMessage: function (message, done) {
+      console.log('message from queue is', message.Body);
+      io.emit("message", message.Body);
+      done();
+    },
+    sqs: new AWS.SQS()
+  });
+  worker.on('error', function (err) {
+    console.log('err from app.js is', err.message);
+  });
+  worker.start();
 });
-worker.on('error', function (err) {
-  console.log('err from app.js is', err.message);
-});
-worker.start();
